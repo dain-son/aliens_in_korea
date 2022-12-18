0532c6bd6d8d2c2f7b8c23b9cdf2ef7a0d8e393e
dain-son <68219415+dain-son@users.noreply.github.com>
2022-12-18 16:57:55 +0900

    Update app.R

--- app.R
+++ app.R
@@ -5,7 +5,7 @@ library(tidyverse)
 library(shiny)
 library(magrittr)
 
-setwd("C:/Users/sea00/Desktop/proj")
+setwd("C:/Users/user/Downloads/drive-download-20221218T044928Z-001")
 
 #외국인유학생
 inter <-read.csv("foreignstudent.csv")
